25961057a627f6e0b70082fb1992e2fa396c744f
Sahad P <iamsahadddd@gmail.com>
2025-10-26 20:38:33 +0530

    updated product.ts file

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -1,24 +1,16 @@
-import { cookies, headers } from "next/headers";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET() {
-  const data = [{ id: 1, title: "T Shirt" }];
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const data = [{ id: 1, title: "T Shirt" }];
+    return res.status(200).json({ data });
+  }
 
-  const cookieList =await cookies();
-  const tokenCookie =cookieList.get("authToken");
-  console.log("tokenCookie:", tokenCookie);
+  if (req.method === "POST") {
+    const prod = req.body;
+    const data = [{ id: 1, title: "T Shirt", prod }];
+    return res.status(200).json({ data });
+  }
 
-  return Response.json({ data });
-}
-
-export async function POST(request: Request) {
-  const prod = await request.json();
-  const cookieList =await cookies();
-  const tokenCookie = cookieList.get("authToken");
-  const headerList = headers();
-
-  console.log("Authorization:", (await headerList).get("Authorization"));
-
-  const data = [{ id: 1, title: "T Shirt", prod }];
-
-  return Response.json({ data });
+  return res.status(405).json({ error: "Method Not Allowed" });
 }
